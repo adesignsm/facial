8c1a983df2ee3b28d7eeb54427142a897dda4dd3
freakolope <akashmulye97@gmail.com>
2020-02-12 17:54:32 -0500

    initial

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var img_upload = document.getElementById("image-upload");
+
+Promise.all([
+	faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+	faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+	faceapi.nets.ssdMobilenetv1.loadFromUri("./models")
+]).then(start)
+
+function start() {
+
+	console.log("loaded");
+}
